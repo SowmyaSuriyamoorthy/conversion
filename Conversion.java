592f43e72392bfefb5eaad5a5d10d2957041f4f3
SowmyaSuriyamoorthy <sowmyaselvi22@gmail.com>
2016-07-19 21:09:31 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Conversion {
+	  public static void main (String[] args)
+	  {
+	  String number = "250000";
+		Integer result = Integer.valueOf(number);		
+		System.out.println(result);
+}
+}
+
+
